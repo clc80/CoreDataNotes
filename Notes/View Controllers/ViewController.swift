0785f004cf20203e03912c16f99fb5424821a189
Claudia Maciel <clc_80@yahoo.com>
2021-09-22 16:29:09 -0700

    Moved CoreData initialization to ViewController

--- Notes/View Controllers/ViewController.swift
+++ Notes/View Controllers/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let coreDataManager = CoreDataManager(modelName: "Notes")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(coreDataManager.managedObjectContext)
     }
 
 
